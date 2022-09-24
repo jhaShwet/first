170796e6f958669569aadc768cb0cf5192c08a74
Shweta jha <111569160+jhaShwet@users.noreply.github.com>
2022-09-24 22:07:42 +0530

    my-first-commit

---
+++ Java/jdk1.8.0_202/bin/MyApplet.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics;
+import java.applet.Applet;
+public class MyApplet extends Applet
+{
+  public void main(Graphics g)
+  {
+     g.drawString("Hello",150,100);
+  }
+}
---
+++ shell_script/os1.sh
@@ -0,0 +1,10 @@
+#os1.sh
+clear
+echo "Shell script";
+ls
+node -v;
+npm -v;
+git --version;
+composer -V;
+echo "ending";
+cat os1.sh
